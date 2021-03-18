05e8ddf0209de996864cb7aeab264d8b50a118dc
peter <byungjoon.choi@solarconnect.kr>
2021-03-18 17:02:35 +0900

    forecast 추가

--- src/weatherbit.service.ts
+++ src/weatherbit.service.ts
@@ -4,10 +4,13 @@ import * as timezone from 'dayjs/plugin/timezone';
 import * as utc from 'dayjs/plugin/utc';
 import { WEATHERBIT_MODULE_CONFIG } from './constants';
 import {
+  EmptyResultException,
   WeatherbitContext,
   WeatherbitCurrentDataForSC,
   WeatherbitCurrentResponse,
   WeatherbitException,
+  WeatherbitForecastDataForSC,
+  WeatherbitForecastResponse,
   WeatherbitLanguage,
   WeatherbitUnit,
 } from './weatherbit.interface';
@@ -93,53 +96,60 @@ export class WeatherbitService {
   }
 
   /**
+   * 좌표로 현재 날씨를 조회합니다. (SAEM 에서 현재 날씨 조회를 목적으로 사용합니다.)
    *
    * @param latitude
    * @param longitude
    */
   async nowByCoordinatesForSC(latitude: number, longitude: number): Promise<WeatherbitCurrentDataForSC> {
-    const response = await this.nowByCoordinates(latitude, longitude);
+    try {
+      const response = await this.nowByCoordinates(latitude, longitude);
 
-    // TODO result to WeatherbitCurrentDataForSC
+      if (response.count < 0) {
+        throw new Error('EmptyResult');
+      }
 
-    if (response.count < 0) {
-      throw new Error('EmptyResult');
-    }
+      const data = response.data[0];
 
-    const data = response.data[0];
+      const now = dayjs().tz('Asia/Seoul');
 
-    const now = dayjs().tz('Asia/Seoul');
+      const datetime = dayjs.utc(data.datetime, 'YYYY-MM-DD HH');
+      const _sunrise = dayjs.tz(dayjs.utc(`${datetime.format('YYYY-MM-DD')} ${data.sunrise}`), 'Asia/Seoul');
+      const _sunset = dayjs.tz(dayjs.utc(`${datetime.format('YYYY-MM-DD')} ${data.sunset}`), 'Asia/Seoul');
 
-    const datetime = dayjs.utc(data.datetime, 'YYYY-MM-DD HH');
-    const _sunrise = dayjs.tz(dayjs.utc(`${datetime.format('YYYY-MM-DD')} ${data.sunrise}`), 'Asia/Seoul');
-    const _sunset = dayjs.tz(dayjs.utc(`${datetime.format('YYYY-MM-DD')} ${data.sunset}`), 'Asia/Seoul');
+      const sunrise = now.set('hour', _sunrise.hour()).set('minutes', _sunrise.minute());
+      const sunset = now.set('hour', _sunset.hour()).set('minutes', _sunset.minute());
 
-    const sunrise = now.set('hour', _sunrise.hour()).set('minutes', _sunrise.minute());
-    const sunset = now.set('hour', _sunset.hour()).set('minutes', _sunset.minute());
+      // icon 이 주간/야간 여부에 상관없이 조회되는 경우가 있어서 이를 보정합니다.
+      let icon: string;
+      if (now.isAfter(sunrise) && now.isBefore(sunset)) {
+        // 주간
+        icon = data.weather.icon.replace(/n/gi, 'd');
+      } else {
+        // 야간
+        icon = data.weather.icon.replace(/d/gi, 'n');
+      }
 
-    // icon 이 주간/야간 여부에 상관없이 조회되는 경우가 있어서 이를 보정합니다.
-    let icon: string;
-    if (now.isAfter(sunrise) && now.isBefore(sunset)) {
-      // 주간
-      icon = data.weather.icon.replace(/n/gi, 'd');
-    } else {
-      // 야간
-      icon = data.weather.icon.replace(/d/gi, 'n');
-    }
-
-    return {
-      original: response,
-      sc: {
-        weather: {
-          icon,
-          code: data.weather.code,
-          description: data.weather.description,
+      return {
+        original: response,
+        sc: {
+          weather: {
+            icon,
+            code: data.weather.code,
+            description: data.weather.description,
+          },
+          sunrise: sunrise.format(),
+          sunset: sunset.format(),
+          temperature: data.temp,
         },
-        sunrise: sunrise.format(),
-        sunset: sunset.format(),
-        temperature: data.temp,
-      },
-    };
+      };
+    } catch (e) {
+      this.logger.error(e);
+      if (e instanceof WeatherbitException) {
+        throw e;
+      }
+      throw new Error('UnhandledError');
+    }
   }
 
   /**
@@ -241,6 +251,83 @@ export class WeatherbitService {
       this.logger.debug(`status: ${status}, statusText: ${statusText}, data: ${JSON.stringify(data)}`);
 
       return data;
+    } catch (e) {
+      this.logger.error(e);
+      if (e instanceof WeatherbitException) {
+        throw e;
+      }
+      throw new Error('UnhandledError');
+    }
+  }
+
+  /**
+   * 좌표로 날씨예보를 조회합니다.
+   * days 값 설정에 따라 최대 16일 까지의 예보 데이터를 조회할 수 있습니다.
+   *
+   * @param latitude
+   * @param longitude
+   * @param days
+   */
+  async forecastDailyByCoordinates(
+    latitude: number,
+    longitude: number,
+    days: number,
+  ): Promise<WeatherbitForecastResponse> {
+    const apiUrl = this.makeUrl(WeatherbitContext.FORECAST_DAILY);
+    this.logger.debug(apiUrl);
+
+    try {
+      const response = await this.httpService
+        .get<WeatherbitForecastResponse>(apiUrl, {
+          params: {
+            lon: longitude,
+            lat: latitude,
+            days,
+          },
+        })
+        .toPromise();
+
+      const { status, statusText, data } = response;
+
+      // TODO 필요 시 WeatherbitException 을 생성합니다.
+
+      this.logger.debug(`status: ${status}, statusText: ${statusText}, data: ${JSON.stringify(data)}`);
+
+      return data;
+    } catch (e) {
+      this.logger.error(e);
+      if (e instanceof WeatherbitException) {
+        throw e;
+      }
+      throw new Error('UnhandledError');
+    }
+  }
+
+  /**
+   * 좌표로 오늘 날씨예보를 조회합니다. (SAEM 에서 최저/최대 기온 조회 목적)
+   *
+   * @param latitude
+   * @param longitude
+   */
+  async forecastDailyByCoordinatesForSC(latitude: number, longitude: number): Promise<WeatherbitForecastDataForSC> {
+    try {
+      const response = await this.forecastDailyByCoordinates(latitude, longitude, 1);
+
+      if (!(Array.isArray(response.data) && response.data.length > 0)) {
+        throw new EmptyResultException();
+      }
+
+      const data = response.data[0];
+
+      const { valid_date, min_temp, max_temp } = data;
+      return {
+        original: response,
+        sc: {
+          date: valid_date,
+          minimumTemperature: min_temp,
+          maximumTemperature: max_temp,
+        },
+      };
     } catch (e) {
       this.logger.error(e);
       if (e instanceof WeatherbitException) {
